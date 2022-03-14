fbe041800533be9602a62b4d319c73b5538ee89d
ILekawa9 <95458978+ILekawa9@users.noreply.github.com>
2022-03-14 06:26:38 -0400

    creation of scrape_mars.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, redirect
+import scrape_mars
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = ('mongodb://localhost:27017/mars_db')
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    mars = mongo.db.images.find_one()
+    return render_template("index.html",mars = mars)
+
+@app.route('/scrape')
+def scrape():
+ 	scrape_mars.scrape()
+ 	return redirect('/', code = 302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
